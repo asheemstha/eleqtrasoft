3049b62a176e73eb7ea2843872e8663ae49fd564
asheemstha <asheem.shrestha01@gmail.com>
2020-10-16 09:27:21 +0545

    Updates

---
+++ precache-manifest.8c42b77d48c39c583d498a3a9da7c767.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a399ab1ec2533827406a91f0d21782",
+    "url": "/eleqtrasoft/index.html"
+  },
+  {
+    "revision": "1741e53f33072d82c3ab",
+    "url": "/eleqtrasoft/static/css/main.8694e0ee.chunk.css"
+  },
+  {
+    "revision": "847ae544aa894d9c136a",
+    "url": "/eleqtrasoft/static/js/2.ea02962d.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/eleqtrasoft/static/js/2.ea02962d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1741e53f33072d82c3ab",
+    "url": "/eleqtrasoft/static/js/main.f97f04fb.chunk.js"
+  },
+  {
+    "revision": "4a5c01070396db64f58f",
+    "url": "/eleqtrasoft/static/js/runtime-main.60db0425.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/eleqtrasoft/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "788e7c705c377d9e08875341f0e860cb",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Bold.788e7c70.woff2"
+  },
+  {
+    "revision": "7b9e40f6db710fa59344e5dc8cc34539",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Bold.7b9e40f6.woff"
+  },
+  {
+    "revision": "3a844470ad64cd09f292369c7841ac9a",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Light.3a844470.woff2"
+  },
+  {
+    "revision": "f5c67c0c4c54897c8b64ac0957fdc364",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Light.f5c67c0c.woff"
+  },
+  {
+    "revision": "61a3f6134b0f3a5eb11b6b740ffcabaa",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Medium.61a3f613.woff"
+  },
+  {
+    "revision": "ed1a889bb737857a86acd44abfc1a9f4",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Medium.ed1a889b.woff2"
+  },
+  {
+    "revision": "5529b729a77573fc6130f2db44a19c77",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Regular.5529b729.woff2"
+  },
+  {
+    "revision": "fbbf6d98bfb9f3410a0de34728e9250e",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-Regular.fbbf6d98.woff"
+  },
+  {
+    "revision": "0afbf8e46c88529a3f140bacd48f5949",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-SemiBold.0afbf8e4.woff2"
+  },
+  {
+    "revision": "7b228459800eddb3e86861952daa4528",
+    "url": "/eleqtrasoft/static/media/SpaceGrotesk-SemiBold.7b228459.woff"
+  }
+]);
